ef1e27bb03f214627599ded878be47ae733523fa
Oleg Davydov <oleg.davydov.dao@gmail.com>
2026-09-01 16:34:03 +0300

    load installed train data

--- src/i_jepa/ijepa.py
+++ src/i_jepa/ijepa.py
@@ -1,8 +1,12 @@
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
+from datasets import load_from_disk
 
 # hyperparameters
 torch.manual_seed(52)
 
 # ImageNet data prepare, data loader
+train_data = load_from_disk('imagenet1k_1000rows')
+print(train_data[:10])
+# images into 224x224px + this happens before installation and save to imagenet1k_1000rows
