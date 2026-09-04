64285cb1573bfe294a43bbd83b4e4f755b900cf0
Oleg Davydov <oleg.davydov.dao@gmail.com>
2026-09-04 13:03:02 +0300

    add transform batch, dataloader, install in main file

--- src/i_jepa/ijepa.py
+++ src/i_jepa/ijepa.py
@@ -1,13 +1,35 @@
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
-from datasets import load_from_disk
+from datasets import load_from_disk, load_dataset, Dataset
 from torchvision.transforms import v2
+from torch.utils.data import DataLoader
+import os
+import sys
 
-# hyperparameters
+# Hyperparameters
 torch.manual_seed(0)
 
-# raw imagenet1k image -> 224x224 crop tensor
+# Install the data only at 1st time
+folder_name = "imagenet1k_1000rows"
+if os.path.isdir(folder_name):
+    print('Data folder is exists')
+else:
+    install_all_data = False
+    print(f'Data folder is missing, starting download from Hugging Face {install_all_data=}')
+    if install_all_data:
+        # install all data next save them. 2x works with 170GB storage, i don't like it.
+        # train_set = load_dataset('ILSVRC/imagenet-1k', split='train', cache_dir=folder_name)
+        # train_set.save_to_disk(folder_name)
+        pass
+    else:
+        num_rows = 1000
+        train_set_stream = load_dataset('ILSVRC/imagenet-1k', split='train', streaming=True) # IterableDataset
+        subset_stream = train_set_stream.take(num_rows)
+        local_dataset = Dataset.from_generator(lambda: iter(subset_stream))
+        local_dataset.save_to_disk(folder_name)
+
+# Raw imagenet1k image -> 224x224 crop tensor
 def make_transform(
     crop_size = 224,
     crop_scale = (0.3, 1.0),
@@ -19,12 +41,33 @@ def make_transform(
         v2.ToDtype(torch.float32, scale=True),
         v2.Normalize(mean=normalization[0], std=normalization[1])
     ])
-    return transform # out = transform(img)
+    return transform
 
-# ImageNet data prepare, data loader
-train_data = load_from_disk('imagenet1k_1000rows')
-img = train_data[0]["image"]
+def transform_batch(n_rows):
+    n_rows["image"] = [transform(img) for img in n_rows["image"]]
+    return n_rows
+
+# I need: 1.28M rows in ROM, xb,yb tensors im RAM.
+# ImageNet data prepare
+train_data = load_from_disk(folder_name)
 transform = make_transform()
-out = transform(img)
-print(img)
-print(out.shape, out.dtype)
+train_data = train_data.with_transform(transform_batch) # when train_data[:batch_size] HF grab and fed it into tranform_batch(.) | with_transform triggers only to slices
+
+# DataLoader | from src/datasets/ i1k.py
+data_loader = DataLoader(
+    train_data,
+    # collate_fn = None, # for masks?: collate_fn = collator = None
+    # collate_fn = default_data_collator, # by gemeni
+    # sampler = dist_sampler,
+    batch_size = 2,
+    # drop_last=True,
+    # pin_memory=True,
+    # num_workers = 1, # 8
+    # persistent_workers=False,
+    # shuffle=True # If sampler specified, shuffle must not be specified
+)
+for batch in data_loader:
+    images = batch["image"]
+    labels = batch["label"]
+    print(images.shape, labels.shape)
+    break
