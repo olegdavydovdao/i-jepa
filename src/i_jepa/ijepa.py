725d9f0c81dc8602420f548ebc70d83357d815aa
Oleg Davydov <oleg.davydov.dao@gmail.com>
2026-09-05 13:55:20 +0300

    from 2 functions of transform to class transform

--- src/i_jepa/ijepa.py
+++ src/i_jepa/ijepa.py
@@ -9,7 +9,12 @@ import sys
 
 # Hyperparameters
 torch.manual_seed(0)
+crop_size = 224,
+crop_scale = (0.3, 1.0)
+normalization = ((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
 
+# Do i need this logic in main page?
+# def if i not install the data and call it, else i have installed data and do not need messy code
 # Install the data only at 1st time
 folder_name = "imagenet1k_1000rows"
 if os.path.isdir(folder_name):
@@ -28,34 +33,33 @@ else:
         local_dataset = Dataset.from_generator(lambda: iter(subset_stream))
         local_dataset.save_to_disk(folder_name)
 
-# Raw imagenet1k image -> 224x224 crop tensor
-def make_transform(
-    crop_size = 224,
-    crop_scale = (0.3, 1.0),
-    normalization = ((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
-):
-    transform = v2.Compose([
+# n_rows of imagenet1k with raw images -> n_rows with 224x224 crop tensor for each image independently
+class Make_transform():
+    def __init__(self, crop_size, crop_scale, normalization):
+        self.transform = v2.Compose([
         v2.ToImage(),
         v2.RandomResizedCrop(crop_size, scale=crop_scale),
         v2.ToDtype(torch.float32, scale=True),
         v2.Normalize(mean=normalization[0], std=normalization[1])
-    ])
-    return transform
+        ])
+    def __call__(self, n_rows):
+        n_rows["image"] = [self.transform(img) for img in n_rows["image"]]
+        return n_rows
 
-def transform_batch(n_rows):
-    n_rows["image"] = [transform(img) for img in n_rows["image"]]
-    return n_rows
-
+# Converting the HF data and mask strategy
 def mask_collator(list_of_i1l1_dicts): # HF Dataset.__getitem__ change the HF type of data
     list_images = [il_dict["image"] for il_dict in list_of_i1l1_dicts]
     xb = torch.stack(list_images)
     return xb
 
 # ImageNet data prepare
+# if else for 1000 rows or full dataset
 train_data = load_from_disk(folder_name)
-transform = make_transform()
-# when train_data[:batch_size] HF grab and fed it into tranform_batch(.) | with_transform triggers only to slices
-train_data = train_data.with_transform(transform_batch)
+
+make_transform = Make_transform(crop_size, crop_scale, normalization)
+def wrap_transform(n_rows):
+    return make_transform(n_rows)
+train_data = train_data.with_transform(wrap_transform)
 
 data_loader = DataLoader(
     train_data,
@@ -64,8 +68,8 @@ data_loader = DataLoader(
 )
 # shuffle or sampler(<- i need it): how it happens and how do it to (image,label) not destroy?
 # num_workers
-# collator: a custom collate_fn so your loop yields explicit xb, yb tuples instead of a dictionary? This would allow you to write for xb, yb in data_loader: directly.
-# default_collator, collate_fn in DataLoader is the thing that mentioned in masking strategy ijepa paper page 12 (gemini said).
+# collate_fn in DataLoader is the thing that mentioned in masking strategy ijepa paper page 12 (gemini said).
+
 for xb in data_loader:
     print(xb.shape)
     print(xb)
