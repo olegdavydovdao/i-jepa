d8933953e9228372966b06e3b4df26fd935b4c27
Oleg Davydov <oleg.davydov.dao@gmail.com>
2026-09-06 09:12:06 +0300

    add Config class

--- src/i_jepa/ijepa.py
+++ src/i_jepa/ijepa.py
@@ -6,15 +6,17 @@ from torchvision.transforms import v2
 from torch.utils.data import DataLoader
 import os
 import sys
-
-# Hyperparameters
+from dataclasses import dataclass
 torch.manual_seed(0)
-crop_size = 224,
-crop_scale = (0.3, 1.0)
-normalization = ((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
-data_folder_name = "imagenet1k_tiny"
-num_train_rows = 1000
-num_val_rows = 100
+
+@dataclass(frozen=True)
+class Config:
+    crop_size: int = 224
+    crop_scale: tuple = (0.3, 1.0)
+    normalization: tuple = ((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+    tiny_data_folder_name: str = "imagenet1k_tiny"
+    num_train_rows: int = 1000
+    num_val_rows: int = 100
 
 # Install small subset of Imagenet1k
 # Note: for full Imagenet code slightly will change due without .save_to_disk())
@@ -23,21 +25,21 @@ def install_data_folder_tiny(split):
     print(f'Data folder {split} is missing, starting download "tiny" Imagenet1k {split} set from Hugging Face')
     stream = load_dataset('ILSVRC/imagenet-1k', split=split, streaming=True) # IterableDataset
     if split == "train":
-        subset_stream = stream.take(num_train_rows)
+        subset_stream = stream.take(cfg.num_train_rows)
     else:
-        subset_stream = stream.take(num_val_rows)
+        subset_stream = stream.take(cfg.num_val_rows)
     local_dataset = Dataset.from_generator(lambda: iter(subset_stream))
-    split_path = f"{data_folder_name}/{split}"
+    split_path = f"{cfg.tiny_data_folder_name}/{split}"
     local_dataset.save_to_disk(split_path)
 
 # n_rows raw images -> n_rows with 224x224 crop tensor for each image independently
 class Make_transform():
-    def __init__(self, crop_size, crop_scale, normalization):
+    def __init__(self):
         self.transform = v2.Compose([
         v2.ToImage(),
-        v2.RandomResizedCrop(crop_size, scale=crop_scale),
+        v2.RandomResizedCrop(cfg.crop_size, scale=cfg.crop_scale),
         v2.ToDtype(torch.float32, scale=True),
-        v2.Normalize(mean=normalization[0], std=normalization[1])
+        v2.Normalize(mean=cfg.normalization[0], std=cfg.normalization[1])
         ])
     def __call__(self, n_rows):
         n_rows["image"] = [self.transform(img) for img in n_rows["image"]]
@@ -51,18 +53,20 @@ class Mask_collator():
         list_images = [il_dict["image"] for il_dict in list_of_i1l1_dicts]
         xb = torch.stack(list_images)
         return xb
-# ----
 
+# --------------------------------------------------------------------------------
+
+cfg = Config()
 # ImageNet_tiny data installation at 1st run
-if os.path.isdir(data_folder_name):
+if os.path.isdir(cfg.tiny_data_folder_name):
     pass
 else:
     install_data_folder_tiny("train")
     install_data_folder_tiny("validation")
 
 # Load and pre-transform train data
-train_data = load_from_disk(f"{data_folder_name}/train")
-make_transform = Make_transform(crop_size, crop_scale, normalization)
+train_data = load_from_disk(f"{cfg.tiny_data_folder_name}/train")
+make_transform = Make_transform()
 def wrap_transform(n_rows): # to num_workers works fine
     return make_transform(n_rows)
 train_data = train_data.with_transform(wrap_transform)
