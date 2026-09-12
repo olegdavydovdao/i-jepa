8943856aa126c13c4cbeac4e28d85bf9701b79e3
Oleg Davydov <oleg.davydov.dao@gmail.com>
2026-09-12 17:44:41 +0300

    fix a bug in readme

--- README.md
+++ README.md
@@ -1,6 +1,26 @@
 # I-JEPA
 Reimplement the I-JEPA paper from scratch.
 
+## Fix a bug in original I-JEPA
+The bug is that the context and target masks never select the last row and last column of the patch grid.\
+For example: in patch grid (14,14). 13 row and 13 column is always 0 and can't be choseen 1 by code.\
+This cause that these patches never used for context or target masks, but still computes.\
+In case (14,14) patch grid 14+13=27 patches is always computes by masking strategy and computes in main model but never selected\
+13.7% computes are wasted and context_size_scale == (0.85, 1.0) is don't work because always get (13,13) and this static image in context.
+
+Path to my code: Mask_collator class/_sample_block_size function
+```python
+while h > cfg.height: # > instead of >=
+    h -= 1
+while w > cfg.width: # > instead of >=
+    w -= 1
+```
+Path to my code: Mask_collator class/_sample_block_mask function
+```python
+top = torch.randint(0, 1+cfg.height - h, (1,)) # (1+cfg.height - h) instead of (cfg.height - h)
+left = torch.randint(0, 1+cfg.width - w, (1,)) # (1+cfg.width - w) instead of (cfg.width - w)
+```
+
 ## Citations
 
 I-JEPA original
--- src/i_jepa/data_prepare.py
+++ src/i_jepa/data_prepare.py
@@ -1,5 +1,4 @@
 import torch
-import torch.nn as nn
 from datasets import load_from_disk, load_dataset, Dataset
 from torchvision.transforms import v2
 from torch.utils.data import DataLoader
@@ -42,12 +41,13 @@ class Make_transform():
         return n_rows
 
 # Collate function and mask strategy
-# My mask strategy is fixed and specialized. To general case masks need add safety code.
+# My mask strategy is fixed and specialized by config. To general case masks need add safety code to do experiments.
 class Mask_collator():
-    def __init__(self):
-        self.finetune = False
+    def __init__(self, finetune=False):
+        self.finetune = finetune
         self._iter_counter = Value('i', -1) # shared int across workers
 
+    # change generator seed for every collate_fn call, each worker increase counter.
     def step(self):
         i = self._iter_counter
         with i.get_lock():
@@ -55,7 +55,7 @@ class Mask_collator():
             v = i.value
         return v
 
-    # sample 1 aspect_ratio and 1 mask_scale to 1 batch
+    # sample 1 aspect_ratio and 1 mask_scale to 1 batch with generator seed
     def _sample_block_size(self, g, mask_scale_range, aspect_ratio_range):
         _rand = torch.rand(1, generator=g).item()
         min_s, max_s = mask_scale_range
@@ -73,7 +73,7 @@ class Mask_collator():
         return (h,w) # max_h == cfg.height
 
     def _sample_block_mask(self, mask_size, masks_t_inv=None):
-        # with targets masks_t_inv=None | with context masks_t_inv=not None
+        # with targets masks_t_inv==None | with context masks_t_inv==not None
         def context_rm_overlap_targets(mask):
             N = len(masks_t_inv)
             for k in range(N):
@@ -87,12 +87,12 @@ class Mask_collator():
         # from top-left corner draw the mask
         mask = torch.zeros((cfg.height, cfg.width), dtype=torch.int32)
         mask[top:top+h, left:left+w] = 1
-        if masks_t_inv is not None:
+        if masks_t_inv is not None: # for context
             mask = context_rm_overlap_targets(mask)
         mask_indices = torch.nonzero(mask.flatten())
         mask_indices = mask_indices.squeeze()
         mask_inverse = None
-        if masks_t_inv is None:
+        if masks_t_inv is None: # for targets
             mask_inverse = torch.ones((cfg.height, cfg.width), dtype=torch.int32)
             mask_inverse[top:top+h, left:left+w] = 0
         return mask_indices, mask_inverse
@@ -107,11 +107,11 @@ class Mask_collator():
         seed = self.step() # seed 0 at the start
         g = torch.Generator().manual_seed(seed)
 
-        # get masks sizes shared to 1 Batch with seed
+        # get masks sizes shared to 1 Batch with generator seed
         target_size = self._sample_block_size(g, cfg.target_mask_scale_range, cfg.target_aspect_ratio_range)
         context_size = self._sample_block_size(g, cfg.context_mask_scale_range, cfg.context_aspect_ratio_range)
 
-        # get masks: locations without seed
+        # get masks: locations without generator seed
         collated_t_idxs, collated_c_idxs = [],[]
         min_keep_target = cfg.num_patches
         min_keep_context= cfg.num_patches
@@ -133,13 +133,13 @@ class Mask_collator():
                 min_keep_context = min(min_keep_context, len(mask_c_indicies))
             collated_c_idxs.append(masks_c_idxs)
 
-        # list of list of 4 tensors. Restrict length of patches to effective GPU batch matrix multiply.
-        collated_t_idxs = [[t_mask[:min_keep_target] for t_mask in list_n_ts] for list_n_ts in collated_t_idxs]
+        # list of list of 4 tensors. Restrict num of patches to be equal over a batch. Cause: effective GPU batch matrix multiply.
+        collated_t_idxs = [[t_mask[:min_keep_target] for t_mask in list_n_ts] for list_n_ts in collated_t_idxs] # in my case is always equal but i want this shield.
         collated_t_idxs = torch.utils.data.default_collate(collated_t_idxs)
         collated_c_idxs = [[c_mask[:min_keep_context] for c_mask in list_n_c] for list_n_c in collated_c_idxs]
         collated_c_idxs = torch.utils.data.default_collate(collated_c_idxs)
 
-        # return list of elements with respect to stage(pretrain or finetune)
+        # return list of elements with respect to stage(pretrain SSL or finetune)
         if self.finetune:
             yb = xbyb_dict["label"]
             return xb, yb
@@ -175,7 +175,7 @@ if __name__=="__main__":
 
     for xb, context_indecies, targets_indecies in data_loader:
         print(xb.shape)
-        # print(context_indecies)
-        # print(targets_indecies)
+        print(context_indecies)
+        print(targets_indecies)
         print(xb)
         break
