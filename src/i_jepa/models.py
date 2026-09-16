846879d5ad98eb9f3f90116188ec7e2546eb39b1
Oleg Davydov <oleg.davydov.dao@gmail.com>
2026-09-16 08:10:25 +0300

    pass classes

---
+++ src/i_jepa/models.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+
+class Attention(nn.Module):
+    pass
+
+class MLP(nn.Module):
+    pass
+
+class Block(nn.Module):
+    pass
+
+class EncoderViT(nn.Module):
+    pass
+
+class PredictorViT(nn.Module):
+    pass
