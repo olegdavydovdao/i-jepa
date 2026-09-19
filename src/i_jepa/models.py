9edd2e45f7cc37ae6f881c728dbdfd8a6a36b2c7
Oleg Davydov <oleg.davydov.dao@gmail.com>
2026-09-19 18:01:30 +0300

    comments in 2d pos_emb funct

--- src/i_jepa/models.py
+++ src/i_jepa/models.py
@@ -13,34 +13,33 @@ class PatchEmbed(nn.Module):
         return x # (B, num_pathces, C)
 
 def get_2d_sincos_pos_embed(cfg):
+    # compute on CPU by default
     h_range = torch.arange(cfg.height, dtype=torch.float32)
     w_range = torch.arange(cfg.width, dtype=torch.float32)
     grid = torch.meshgrid(h_range, w_range, indexing='ij')
-    grid = torch.stack(grid).view(2,1,cfg.height,cfg.width)
-    # print(grid)
-    # sys.exit(0)
+    grid = torch.stack(grid)
     pos_embed = get_2d_sincos_pos_embed_from_grid(cfg.emb_dims, grid)
     return pos_embed
 
 def get_2d_sincos_pos_embed_from_grid(emb_dims, grid):
     assert emb_dims % 2 == 0
     half_emb_dims = emb_dims//2
-    emb_h = get_1d_sincos_pos_embed_from_grid(half_emb_dims, grid[1])
-    emb_w = get_1d_sincos_pos_embed_from_grid(half_emb_dims, grid[0])
-    emb = torch.cat((emb_h, emb_w), dim=1)
+    emb_h = get_1d_sincos_pos_embed_from_grid(half_emb_dims, grid[1]) # (N, D//2)
+    emb_w = get_1d_sincos_pos_embed_from_grid(half_emb_dims, grid[0]) # (N, D//2)
+    emb = torch.cat((emb_h, emb_w), dim=1) # (N, D)
     return emb
 
 def get_1d_sincos_pos_embed_from_grid(half_emb_dims, pos):
+    pos = pos.view(-1,1) # (N, 1)
     assert half_emb_dims % 2 == 0
     quarter_emb_dims = half_emb_dims//2
-    omega = torch.arange(quarter_emb_dims, dtype=torch.float32)
+    omega = torch.arange(quarter_emb_dims, dtype=torch.float32).view(1,-1)
     omega /= quarter_emb_dims
-    omega = 1.0 / 10000**omega
-    pos = pos.view(-1,1)
-    out = pos * omega
+    omega = 1.0 / 10000**omega # (1, D//4)
+    out = pos * omega # (N, D//4)
     emb_sin = torch.sin(out)
     emb_cos = torch.cos(out)
-    emb = torch.cat((emb_sin, emb_cos), dim=1)
+    emb = torch.cat((emb_sin, emb_cos), dim=1) # (N, D//2)
     return emb
 
 class Attention(nn.Module):
@@ -57,8 +56,8 @@ class EncoderViT(nn.Module):
         super().__init__()
         self.patch_embed = PatchEmbed(cfg)
         self.pos_embed = nn.Parameter(torch.zeros(1, cfg.num_patches, cfg.emb_dims), requires_grad=False)
-        pos_embed = get_2d_sincos_pos_embed(cfg)
-        print(pos_embed.shape)
+        pos_embed = get_2d_sincos_pos_embed(cfg) # (N,D) | on CPU
+        # print(pos_embed.shape)
         sys.exit(0)
         
     def forward(self, x, masks=None): # x is (B,3,224,224)
